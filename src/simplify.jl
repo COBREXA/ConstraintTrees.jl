52c96ad5afcc7d60b6918916c0a8ed9ab16a4328
Mirek Kratochvil <exa.exa@gmail.com>
2025-06-20 14:10:30 +0200

    stash

--- src/simplify.jl
+++ src/simplify.jl
@@ -52,12 +52,18 @@ C.squared(x::Interval) =
         Interval(x.low <= 0 && x.high >= 0 ? 0 : min(a, b), max(a, b))
     end
 
+Interval{T}(x::Nothing) = Interval{T}(typemin(T), typemax(T))
+Interval(x::EqualToT{T}) where {T} = Interval{T}(x.equal_to, x.equal_to)
+Interval(x::BetweenT{T}) where {T} = Interval{T}(x.lower, x.upper)
+
 mutable struct VariableState{T}
     subst::Union{Nothing,C.Value} # replacement
     bound::Interval{T} # implied bound
     occurs::Set{Int} # constraint indexes (TODO sorted?)
     changed::Int
 end
+
+isactive(x::VariableState) = !(isnothing(x.subst) || isempty(x.occurs))
 
 mutable struct ConstraintState{T}
     value::C.Value
@@ -66,16 +72,18 @@ mutable struct ConstraintState{T}
     changed::Int
 end
 
+isactive(x::ConstraintState{T}) where {T} =
+    !(x.bound.low == typemin(T) && x.bound.high == typemax(T))
+
 mutable struct SimplifierState{T}
-    tree::C.ConstraintTree
     vs::Vector{VariableState{T}}
     cs::Vector{ConstraintState{T}}
 end
 
 abstract type SimplifierResult end
-struct SimplifierWorked end
-struct SimplifierNoop end
-struct SimplifierError
+struct SimplifierWorked <: SimplifierResult end
+struct SimplifierNoop <: SimplifierResult end
+struct SimplifierError <: SimplifierResult
     msg::String
     problem::ConstraintTree
 end
@@ -85,25 +93,43 @@ end
 #
 
 function check_bounds!(s::SimplifierState{T}, last::Int, vtime::Int) where {T}
-    # do nothing but throw error if any of the intervals is empty
-    return SimplifierNoop()
-end
-
-function imply_variable_bounds!(s::SimplifierState{T}, last::Int, vtime::Int) where {T}
-    # go through the constraints that perhaps touch the variable and try to
-    # tighten the interval
+    for (i, v)=enumerate(s.vs)
+        v.bound.low <= v.bound.high && continue
+        return SimplifierError("variable $i bounded to empty domain", ConstraintTree())
+    end
+    # TODO this might not be ever required since the bounds of constraints generally grow
+    for (i, c) = enumerate(s.cs)
+        c.bound.low <= c.bound.high && continue
+        return SimplifierError("constraint domain became empty", foldr(^, c.address, init=Constraint(c.value)))
+    end
     return SimplifierNoop()
 end
 
 function dominate_constraint_bounds!(s::SimplifierState{T}, last::Int, vtime::Int) where {T}
     # if the bound implied by variables is smaller than the constraint bound in
     # any direction, remove the constraint bound
+    bounds = [v.bound for v = s.vs]
+    did_work=false
+    for c = s.cs
+        implied_bound = C.substitute(c.value, bounds)
+        # drop bounds if possible
+        # TODO we can drop unnecessary bounds at the end, shouldn't we tighten them here instead?
+        if implied_bound.low > c.bound.low
+            c.bound.low = typemin(T)
+        end
+        if implied_bound.high < c.bound.high
+            c.bound.high = typemax(T)
+        end
+        if implied_bound.low > c.bound.high || implied_bound.high < c.bound.low
+            return SimplifierError("constraint domain insatisfiable, implied to $implied_bound", foldr(^, c.address, init=Constraint(c.value, C.BetweenT(c.bound.low, c.bound.high))))
+        end
+        # TODO should we detect and convert equalities here, to be able to invert more stuff?
+    end
     return SimplifierNoop()
 end
 
 function drop_constraint_bounds!(s::SimplifierState{T}, last::Int, vtime::Int) where {T}
     # remove constraints where the bound is infinite
-    # adjust all indexes in occurences
     return SimplifierNoop()
 end
 
@@ -112,12 +138,37 @@ function sparsify_constraints!(s::SimplifierState{T}, last::Int, vtime::Int) whe
     return SimplifierNoop()
 end
 
-function solve_singletons!(s::SimplifierState{T}, last::Int, vtime::Int) where {T}
-    # find a singleton equality, and substitute for the variable
+function imply_variable_singleton_bounds!(s::SimplifierState{T}, last::Int, vtime::Int) where {T}
+    # go through the simple that perhaps touch the variable and try to
+    # tighten the interval
     return SimplifierNoop()
 end
 
+function drop_fixed_variables!(s::SimplifierState{T}, last::Int, vtime::Int) where {T}
+    # substitute a constant for a fixed variable
+    # TODO might deserve its own tolerance
+    did_work = false
+    for(i,v)=enumerate(s.vs)
+        isactive(v) || continue
+        v.bound.low != v.bound.high && continue
+        # TODO add a substitution
+    end
+    return did_work ? SimplifierWorked() : SimplifierNoop()
+end
+
 end # module Simplifier
+
+abstract type SimplifyResult end
+struct SimplifyOK <: SimplifyResult
+    constraints::ConstraintTree
+    variables::Vector{Value}
+    output::ConstraintTree
+    iterations::Int
+end
+struct SimplifyFailed <: SimplifyResult
+    msg::String, problem::ConstraintTree
+    SimplifyFailed(x::Simplifier.SimplifyResult) = new(x.msg, x.problem)
+end
 
 """
 $(TYPEDSIGNATURES)
@@ -127,14 +178,15 @@ TODO
 function simplify(
     ct::ConstraintTree,
     carrier_type::Type{T};
-    methods = (
-        Simplifier.simplify_variable_bounds!,
-        Simplifier.simplify_constraint_bounds,
-        Simplifier.imply_variable_bounds!,
+    simplifiers = (
+        Simplifier.check_bounds!,
+        Simplifier.imply_variable_bounds,
         Simplifier.dominate_constraint_bounds!,
-        Simplifier.solve_constants!,
+        Simplifier.drop_constraint_bounds!,
+        Simplifier.sparsify_constraints!,
         Simplifier.solve_singletons!,
     ),
+    iteration_limit = 100,
 ) where {T<:Real}
     #TODO (notes below)
     # From looking at the other simplifiers/presolvers, we should support:
@@ -161,23 +213,70 @@ function simplify(
     # Notably, we don't really need the "postsolve" here, because the CTs can
     # effectively carry the postsolve information all by itself.
 
-    # collect variable state
-    # TODO
+    state = let
+        # collect variable state
+        vs = [
+            VariableState{T}(
+                C.variable(one(T); idx = i),
+                bound = Interval(nothing),
+                Set{Int}(),
+                0,
+            ) for i = 1:var_count(ct)
+        ]
 
-    # filter out all constraints that still do anything, put them into a separate tree
-    # TODO
+        # filter out constraints that do anything, put them into a separate tree
+        n_constraints = 0
+        traverse(ct) do c
+            if !isnothing(c.bound)
+                n_constraints += 1
+            end
+        end
+        cs = Vector{ConstraintState{T}}(undef, n_constraints)
+        itraverse(ct) do path, c
+            if isnothing(c.bound)
+                return
+            end
+            cs[n_constraints] =
+                ConstraintState{T}(c.value, Interval{T}(c.bound), path, 0)
+            n_constraints -= 1
+        end
+        @assert n_constraints == 0
 
-    # initial simplifier state
-    # TODO
+        # TODO update variable occurences!
 
-    # run simplifiers until they all Noop or until there's error, or until
-    # there's some kind of iteration limit hit
-    # TODO
+        # initial simplifier state
+        SimplifierState{T}(vs, cs)
+    end
+
+    # run simplifiers until they all Noop or until there's error
+    last_vtime = zeros(Int, length(simplifiers))
+    vtime = 1
+    iteration = 0
+    while iteration < iteration_limit
+        iteration += 1
+        bump_flag = false
+        for (si, s) in enumerate(simplifiers)
+            res = s(state, last_vtime[si], vtime)
+            if res isa SimplifierError
+                return SimplifyFailed(res)
+            elseif res isa SimplifierNoop
+                continue
+            elseif res isa SimplifierWorked
+                vtime += 1
+                bump_flag = true
+                continue
+            end
+            @assert false "we shouldn't get here"
+        end
+
+        bump_flag || break # no more stuff to do, terminate early early
+    end
 
     # purge constraints from the original tree
-    # TODO
+    subst = TODO
+    sct = map(ct) do
+        C.Constraint(substitute(ct.value, subst)) # all bounds omitted!
+    end
 
-    # TODO We should have a return value that prevents people from neglecting
-    # the simplifier error check
-    return
+    return SimplifyOK(TODO, TODO, sct, iteration)
 end
